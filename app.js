293dff36ec66652686056bee2fc7df50dcc6f64e
“Rajul” <rajulkoshta10@gmail.com>
2021-09-11 20:58:51 +0530

    added icons and updates files

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
-let birthDay = document.querySelector("#birthday");
-let luckyNumber = document.querySelector("#luckyNum");
-let checkBtn = document.querySelector(".btn-primary");
-let reloadBtn = document.querySelector(".btn-secondary");
-let message = document.querySelector("#error-message");
-let birthDayvalueWithoutHyphen = birthDay.value.replaceAll('-','');
-let birthDayString = birthDayvalueWithoutHyphen.toString();
+const birthDay = document.querySelector("#birthday");
+const luckyNumber = document.querySelector("#luckyNum");
+const checkBtn = document.querySelector(".btn-primary");
+const reloadBtn = document.querySelector(".btn-secondary");
+const message = document.querySelector("#message");
 
 
-let sum=0;
-
-
-checkBtn.addEventListener("click", checkBirthday);
-function checkBirthday(){ 
-      console.log(birthDayvalueWithoutHyphen);
-    console.log(birthDayString);
+function checkBirthday(){
+    hideMessage(); 
+    const dob= birthDay.value;
+    const luckyno = luckyNumber.value;
+    const sum = calculateSum(dob);
+    if(sum % luckyno == 0){
+        showMessage("Congratulations your birthday is lucky🥳");
+    }
+    else{
+        showMessage("Sorry according to the logic "+luckyno+" is not that lucky😟")
+    }
 }
 
+function calculateSum(dob){
+        dob = dob.replaceAll('-','');
+        let sum=0;
+        for(var index=0 ; index < dob.length ; index++){
+            sum = sum + Number(dob.charAt(index));
+        }
+        return sum;
+}
+
+checkBtn.addEventListener("click", checkBirthday);
 
 
+function hideMessage() {
+    message.style.display = "none";
+}
+
+function showMessage(msg) {
+    message.style.display = "block";
+    message.innerText =msg;
+
+}
 
 
 reloadBtn.addEventListener("click", refresh);
